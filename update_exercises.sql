002eba7c92df68a0859e20ee0805dfece26876c6
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-26 14:31:26 -0500

    finished update section

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,7 +1,10 @@
 USE codeup_test_db;
 
 SELECT * FROM albums;
+UPDATE albums SET sales = sales * 10;
 
 SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums SET release_date = release_date -100 WHERE release_date < 1980;;
 
-SELECT * from albums where artist = 'Michael Jackson';
+SELECT * FROM albums where artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
