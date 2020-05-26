fa905e56624f12813e84fe8759cd44ea04086e77
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-26 14:14:24 -0500

    finished exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -7,3 +7,7 @@ SELECT release_date AS 'Release Date of Sgt. Pepper' FROM albums WHERE id = 48;
 SELECT genre AS 'Genre of Nevermind by Nirvana' FROM albums WHERE artist = 'Nirvana' AND name = 'Nevermind';
 
 SELECT Name , Artist, release_date AS 'release year' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT name AS 'Albums that sold 20 million or less' FROM albums WHERE sales <= 20;
+
+SELECT name AS 'Albums with a genre of Rock' FROM albums WHERE  genre = 'Rock';
