6f68aa1aca395a0e4b09ced9829efbbcd0239852
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-26 14:02:23 -0500

    finished second step

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,6 @@
 USE codeup_test_db;
 
 SELECT name AS 'Albums by Pink Floyd' FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT release_date AS 'Release Date of Sgt. Pepper' FROM albums WHERE id = 48;
+
