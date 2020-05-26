2003bdeec4b5c236cc977c484bf50b9dc72b3e5e
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-26 12:15:07 -0500

    fixed everything

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,15 +1,12 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums (
+CREATE table albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist varchar() NOT NULL,
-    name varchar () NOT NULL,
-    release_date INT() UNSIGNED,
-    sales FLOAT () UNSIGNED,
-    genre varchar() NOT NULL,
+    artist varchar(250) NOT NULL,
+    name varchar (300) NOT NULL,
+    release_date INT(4) UNSIGNED,
+    sales FLOAT (5,2) UNSIGNED,
+    genre varchar(300),
     PRIMARY KEY (id),
     UNIQUE (artist, name)
-
-
-
 );
