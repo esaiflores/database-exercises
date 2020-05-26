f5091eb925ce3c4f071cfb0e901b74da361e38c1
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-26 09:35:02 -0500

    finished exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,6 +1,14 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist varchar() NOT NULL,
+    name varchar () NOT NULL,
+    release_date INT() UNSIGNED,
+    sales FLOAT () UNSIGNED,
+    genre varchar() NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (artist, name)
 
 
 
