48757ba5bcd10e34f0717347e2dd86b340337427
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-26 14:06:25 -0500

    finished fourth step

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,3 +5,5 @@ SELECT name AS 'Albums by Pink Floyd' FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date AS 'Release Date of Sgt. Pepper' FROM albums WHERE id = 48;
 
 SELECT genre AS 'Genre of Nevermind by Nirvana' FROM albums WHERE artist = 'Nirvana' AND name = 'Nevermind';
+
+SELECT Name , Artist, release_date AS 'release year' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
