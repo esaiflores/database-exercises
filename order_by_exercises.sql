88e0439649c74558f6e64c53cbd47db6ecd06afc
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-27 16:44:18 -0500

    born in the 90s and Christmas

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -32,3 +32,9 @@ FROM employees
 WHERE last_name LIKE ('E%')
   AND last_name like('%E')
 ORDER BY emp_no DESC;
+
+SELECT emp_no, first_name, last_name, birth_date, hire_date
+FROM employees
+WHERE hire_date like ('199%')
+  AND birth_date like ('%12-25')
+ORDER BY birth_date ASC, hire_date DESC;
