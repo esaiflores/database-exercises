5f82eaae7b7c38b3da206984d5ab93c9011c20ad
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-27 16:42:18 -0500

    updated for employees with letter E

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -15,3 +15,8 @@ SELECT emp_no, first_name, last_name
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY last_name, first_name;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE ('E%')
+ORDER BY emp_no DESC;
