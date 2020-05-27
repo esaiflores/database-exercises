dfd85a05f071f44facfbec2d6608a394691f5196
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-27 14:10:56 -0500

    finished #3

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,4 +6,4 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 SELECT *
 FROM employees
-WHERE (first_name = 'Irena') Or (first_name = 'Vidya') Or (first_name = 'Maya');
+WHERE last_name LIKE ('E%');
