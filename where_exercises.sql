c35eaed673c9222c149a90bbe39de1e7707b7936
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-27 15:09:52 -0500

    finished

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,11 +4,31 @@ SELECT *
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena') Or (first_name = 'Vidya') Or (first_name = 'Maya');
+
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya') AND gender = 'M';
+
 
 SELECT *
 FROM employees
 WHERE last_name LIKE ('E%');
 
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%')
+   OR last_name like('%E');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%')
+  AND last_name like('%E');
+# or WHERE last_name LIKE ('E%E');
 
 
 
