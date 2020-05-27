7ccb4d6d6e52bb78d1662986f4f8977a5526eaa4
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-27 14:04:24 -0500

    finished #2

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,8 +1,8 @@
 USE codeup_test_db;
 -- TRUNCATE albums;
 
-INSERT INTO albums (artist,name,release_date,sales,genre)
-VALUES ('Michael Jackson', 'Thriller', 1982,   'Pop, Rock, R&B',   47.3),
+insert into albums (artist,name,release_date,sales,genre)
+values ('Michael Jackson', 'Thriller', 1982,   'Pop, Rock, R&B',   47.3),
 ('AC/DC',   'Back in Black',    1980,   'Hard rock',    26.1),
 ('Pink Floyd',  'The Dark Side of the Moon',    1973,   'Progressive rock', 24.2),
 ('Meat Loaf',   'Bat Out of Hell',  1977,   'Hard rock, Progressive rock',  21.5),
