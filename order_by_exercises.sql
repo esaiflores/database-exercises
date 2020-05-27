cc4fbca1960a40aed1cdc8ea7f469af1141c8aa0
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-27 16:43:42 -0500

    reverse sort order

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -20,3 +20,15 @@ SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE ('E%')
 ORDER BY emp_no DESC;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE ('E%')
+   OR last_name like('%E')
+ORDER BY emp_no DESC;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE ('E%')
+  AND last_name like('%E')
+ORDER BY emp_no DESC;
