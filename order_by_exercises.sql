304d6c9867af119e51db4df848fb6ceffc1c5495
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-27 16:41:04 -0500

    first name last name order

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,3 +4,8 @@ SELECT emp_no, first_name, last_name
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
