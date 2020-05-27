272b0809e5b7b1ef134a050bdd408967733253b2
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-27 14:09:36 -0500

    finished #3

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,3 +3,7 @@ USE employees;
 SELECT *
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena') Or (first_name = 'Vidya') Or (first_name = 'Maya');
