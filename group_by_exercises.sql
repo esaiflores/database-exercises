ded0a8b6350300f82fb8989bc156ea09c7a6fdf4
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-28 14:28:20 -0500

    updated last feat

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -7,3 +7,8 @@ SELECT last_name
 FROM employees
 WHERE last_name LIKE ('E%E')
 group by last_name;
+
+SELECT last_name, first_name
+FROM employees
+WHERE last_name LIKE ('E%E')
+group by last_name, first_name;
