29479c2b431b11c58e6a5250735a7cbde9911bb6
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-28 14:29:18 -0500

    feat: fourth step

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,3 +12,9 @@ SELECT last_name, first_name
 FROM employees
 WHERE last_name LIKE ('E%E')
 group by last_name, first_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name like ('%q%')
+  AND last_name NOT LIKE ('%qu%')
+group by last_name;
