70e8203c190194b297070c7e1d36f9adec4ca24f
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-31 21:58:55 -0500

    finished exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -23,3 +23,11 @@ from employees
               on employees.emp_no = dept_emp.emp_no
 WHERE dept_emp.to_date > now() and dept_no = 'd009' and titles.to_date > now()
 group by titles.title;
+
+SELECT departments.dept_name AS 'Department Name', CONCAT_WS(' ',e.first_name, e.last_name) AS 'Department Manager',salaries.salary as 'Salary'
+FROM employees as e
+         JOIN dept_manager ON e.emp_no = dept_manager.emp_no
+         JOIN departments on dept_manager.dept_no = departments.dept_no
+         JOIN salaries on salaries.emp_no = dept_manager.emp_no
+WHERE dept_manager.to_date > now() and salaries.to_date > now()
+ORDER BY dept_name;
