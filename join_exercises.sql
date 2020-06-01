9c86baa541333a670bcc794907d4e5a29bfa544e
Esai Flores <flores.esaijonathan@gmail.com>
2020-06-01 12:20:23 -0500

    finished first step

--- join_exercises.sql
+++ join_exercises.sql
@@ -10,7 +10,7 @@ ORDER BY dept_name;
 SELECT departments.dept_name AS 'Department Name', CONCAT_WS(' ',e.first_name, e.last_name) AS 'Department Manager'
 FROM employees as e
          JOIN dept_manager ON e.emp_no = dept_manager.emp_no
-         JOIN departments on dept_manager.dept_no = departments.dept_no
+         JOIN departments on dept_manager.dept_no = departments.d7ept_no
 WHERE to_date > now()
   AND gender ='f'
 ORDER BY dept_name;
---
+++ subqueries_exercises.sql
@@ -0,0 +1,4 @@
+use employees;
+
+select * from employees
+where emp_no = 101010;
