02a8b21c86b5a53eed11699fbebc9f27aba23e8f
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-31 21:57:25 -0500

    finished first exercise

---
+++ join_exercises.sql
@@ -0,0 +1,8 @@
+use employees;
+
+SELECT departments.dept_name AS 'Department Name', CONCAT_WS(' ',e.first_name, e.last_name) AS 'Department Manager'
+FROM employees as e
+         JOIN dept_manager ON e.emp_no = dept_manager.emp_no
+         JOIN departments on dept_manager.dept_no = departments.dept_no
+WHERE to_date > now()
+ORDER BY dept_name;
