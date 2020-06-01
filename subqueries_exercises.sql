e53f1c22e3dfdce013d1621b245e257c72ba55c3
Esai Flores <flores.esaijonathan@gmail.com>
2020-06-01 12:21:10 -0500

    second step done

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,3 +2,18 @@ use employees;
 
 select * from employees
 where emp_no = 101010;
+
+select concat_ws(' ',first_name,last_name) as 'first and last name', hire_date
+from employees
+where hire_date =(
+    select hire_date from employees
+    where emp_no = 101010
+);
+
+select *
+from titles
+where emp_no IN (
+    select emp_no
+    from employees
+    where first_name = 'Aamod'
+);
